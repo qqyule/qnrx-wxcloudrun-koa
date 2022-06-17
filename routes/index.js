cc2f00ab5ddbe7cd89465e8f0097f43905d056fb
谦哥 <qqyule@gmail.com>
2022-06-17 16:29:42 +0800

    update

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const router = require("koa-router")();
+// const { init: initDB, Counter } = require("./db");
+
+router.get("/ping", async (ctx, next) => {
+  await ctx.render("index", {
+    title: "Tencent CloudBase + Koa",
+  });
+});
+
+// router.post("/api/count", async (ctx) => {
+//   const { request } = ctx;
+//   const { action } = request.body;
+//   if (action === "inc") {
+//     await Counter.create();
+//   } else if (action === "clear") {
+//     await Counter.destroy({
+//       truncate: true,
+//     });
+//   }
+
+//   ctx.body = {
+//     code: 0,
+//     data: await Counter.count(),
+//   };
+// });
+
+module.exports = router;
