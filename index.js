0223fc4491607ee49b8968cc077e32278bf6de65
谦哥 <qqyule@gmail.com>
2022-06-25 10:22:34 +0800

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app
   .use(bodyParser())
 
 app.use(index.routes(), index.allowedMethods())
-app.use(scan.routes(), index.allowedMethods())
+app.use(scan.routes(), scan.allowedMethods())
 const port = process.env.PORT || 80;
 async function bootstrap() {
   await initDB();
